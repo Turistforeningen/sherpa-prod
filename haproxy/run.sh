3fdefa565a19f5bef47d38b1341fdbe19bb8ae3a
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-05-05 16:22:08 +0200

    New line sperate HAProxy run command

--- haproxy/run.sh
+++ haproxy/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run -d -p 0.0.0.0:80:80 -p 0.0.0.0:443:443 --name=haproxy --restart=always sherpa/haproxy
+docker run -d \
+  --name=haproxy \
+  --restart=always \
+  --add-host docker0:172.17.42.1 \
+  -p 0.0.0.0:80:80 \
+  -p 0.0.0.0:443:443 \
+  sherpa/haproxy
