3dc75b347562ab88ffe81618e9882327eaaa4d2a
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-05-06 18:36:42 +0200

    Update Sherpa run script

--- sherpa/run.sh
+++ sherpa/run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-SHA=62bc4e9
-PORT=8000
-
-docker run --name static_${SHA} sherpa/static:${SHA} /bin/echo
-
-docker run -d --restart=always --name memcached_${SHA} memcached:1.4
-docker run -d --restart=always --name django_${SHA} --link
-memcached_${SHA}:memcached sherpa/django:${SHA}
-docker run -d --restart=always --name nginx_${SHA} \
-  --publish ${PORT}:8080 \
-  --link django_${SHA}:sherpa \
-  --volumes-from static_${SHA} \
-  sherpa/nginx:${SHA}
+SHA=`cd sherpa; git log -n 1 --pretty=format:'%h' --abbrev-commit`
+docker-compose -f sherpa/docker-compose-prod.yml -p ${SHA} up -d
+docker-compose -f sherpa/docker-compose-prod.yml -p ${SHA} ps
