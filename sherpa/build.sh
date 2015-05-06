728a4f8574bedeeb02bd1e75b5ee9c44e7027045
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-05-06 17:47:31 +0200

    Update Sherpa build script

--- sherpa/build.sh
+++ sherpa/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker build -t sherpa/django:62bc4e9 -f sherpa/Dockerfile sherpa/
-docker build -t sherpa/static:62bc4e9 -f sherpa/static/Dockerfile.static sherpa/static/
-docker build -t sherpa/nginx:62bc4e9  -f sherpa/Dockerfile.nginx sherpa/build/
+SHA=`cd sherpa; git log -n 1 --pretty=format:'%h' --abbrev-commit`
+docker-compose --verbose -f sherpa/docker-compose-prod.yml -p ${SHA} build
