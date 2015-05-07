32a5c368aec7e457313ef82f5df1f4ef23acd58f
Ali Kaafarani <ali@kvikshaug.no>
2015-05-07 16:42:14 +0200

    Rephrase after preroute -> output change

--- haproxy/route-backend.sh
+++ haproxy/route-backend.sh
@@ -5,14 +5,14 @@ OLD_PORTS=`iptables -t nat -S | grep "\-A OUTPUT" | sed -r 's/^.+ --to-destinati
 
 # Add new port to preroute
 if [ -n "${NEW_PORT}" ]; then
-  echo "Adding backend port '${NEW_PORT}' to prerouting..."
+  echo "Adding backend port '${NEW_PORT}' to NAT redirects..."
   iptables -t nat -A OUTPUT -p tcp --dport 8080 -j DNAT --to-destination :${NEW_PORT}
 fi
 
 # Remove the old ports
 if [ -n "${OLD_PORTS}" ]; then
   while read -r OLD_PORT; do
-    echo "Removing old backend port '${OLD_PORT}' from prerouting..."
+    echo "Removing old backend port '${OLD_PORT}' from NAT redirects..."
     iptables -t nat -D OUTPUT -p tcp --dport 8080 -j DNAT --to-destination :${OLD_PORT}
   done <<< "${OLD_PORTS}"
 fi
