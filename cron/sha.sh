bc6d4a7ad47007ef646754f2780e82238f86a578
Ali Kaafarani <ali@kvikshaug.no>
2015-05-12 15:54:20 +0200

    Handle all submodule status prefixes

--- cron/sha.sh
+++ cron/sha.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /cron/sherpa-prod
-FULL_SHA=$(git submodule status sherpa | awk '{ print $1 }' | sed 's/^-//')
+FULL_SHA=$(git submodule status sherpa | awk '{ print $1 }' | sed 's/^[-+U]//')
 echo ${FULL_SHA::7}
