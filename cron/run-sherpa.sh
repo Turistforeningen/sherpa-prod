732c064bcfb1b4605ba370c0b9f0728442fe69ac
Ali Kaafarani <ali@kvikshaug.no>
2015-05-12 13:59:49 +0200

    Log cronjob output to file

--- cron/run-sherpa.sh
+++ cron/run-sherpa.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 /cron/pull.sh
-docker-compose -f /cron/sherpa-prod/sherpa/sherpa/docker-compose-prod.yml -p $(/cron/sha.sh) run --rm sherpa $@
+echo
+echo "$(date): Running on sherpa@$(/cron/sha.sh): $@" >> /cron/logfile 2>&1
+docker-compose -f /cron/sherpa-prod/sherpa/sherpa/docker-compose-prod.yml -p $(/cron/sha.sh) run --rm sherpa $@ >> /cron/logfile 2>&1
