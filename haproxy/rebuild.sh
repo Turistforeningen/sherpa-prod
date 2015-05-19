f42f838f0a93fce0246f6f294f18e140f49cf680
Ali Kaafarani <ali@kvikshaug.no>
2015-05-19 09:01:50 +0200

    Look up port automatically if none specified on rebuild

--- haproxy/rebuild.sh
+++ haproxy/rebuild.sh
@@ -2,8 +2,17 @@
 PORT=$1
 
 if [ -z ${PORT} ]; then
-  echo "usage: $0 [PORT]"
-  exit 1
+  echo "No port specified, looking up current port in haproxy container..."
+  PORT=`docker exec haproxy /bin/bash -c "iptables -t nat -S | grep \"\-A OUTPUT\" | sed -r 's/^.+ --to-destination :([0-9]+)$/\1/'"`
+  read -p "Rebuilding with backend route to port '${PORT}', OK? [y/N] " yn
+  case $yn in
+    [Yy]*)
+      ;;
+    *)
+      echo "usage: $0 [PORT]"
+      exit 1
+      ;;
+  esac
 fi
 
 ./build.sh
