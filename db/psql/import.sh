f61e22077c1aac3945b6d741551eb0501bb8b287
Ali Kaafarani <ali@kvikshaug.no>
2015-06-29 16:12:46 +0200

    Allow argument to import specific database file

--- db/psql/import.sh
+++ db/psql/import.sh
@@ -3,7 +3,9 @@ PSQL="gosu postgres psql -h postgres"
 PGDIR=/usr/share/postgresql/$PG_MAJOR/contrib/postgis-$POSTGIS_MAJOR
 
 s3_path="s3://${AWS_S3_BUCKET_NAME}${AWS_S3_BUCKET_PATH}"
-s3_file=`aws s3 ls ${s3_path} | tail -n 1 | awk ' {print $4}'`
+s3_file=$1
+# Default to the latest backup if no argument specified
+: ${s3_file:=`aws s3 ls ${s3_path} | tail -n 1 | awk ' {print $4}'`}
 
 echo "Downloading '${s3_file}' from S3..."
 aws s3 cp "${s3_path}${s3_file}" sherpa.gz
