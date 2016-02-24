8ec099ccd5a3fc1027a3ffc5a64fc0c460748b16
Ali Kaafarani <ali@kvikshaug.no>
2016-02-24 11:36:56 +0100

    Print correct status code

--- cron/run-sherpa.sh
+++ cron/run-sherpa.sh
@@ -4,4 +4,5 @@ echo -e "\n$(date): pulling sherpa-prod..." >> /cron/logfile 2>&1
 
 echo -e "\n$(date): Running on sherpa@$(/cron/sha.sh): $@" >> /cron/logfile 2>&1
 docker-compose -f /cron/sherpa-prod/sherpa/sherpa/production.yml -p $(/cron/sha.sh) run --rm sherpa $@ >> /cron/logfile 2>&1
-echo -e "\n$(date): Process exited with status code $?" >> /cron/logfile 2>&1
+STATUS_CODE=$?
+echo -e "\n$(date): Process exited with status code ${STATUS_CODE}" >> /cron/logfile 2>&1
