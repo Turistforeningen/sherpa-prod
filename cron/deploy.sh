cf2c4b2cd221b852dc442952ef355b1b68fa0beb
Ali Kaafarani <ali@kvikshaug.no>
2016-08-16 10:11:38 +0200

    Fix cron deploy

--- cron/deploy.sh
+++ cron/deploy.sh
@@ -4,12 +4,15 @@
 pushd "$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) )" > /dev/null
 
 echo "Updating cron/sherpa repo..."
-pushd cron
-git pull
+pushd cron/sherpa
+git fetch origin
+git checkout master
+git reset --hard origin/master
 git submodule update --init --recursive
 popd
 
 echo "Rebuilding Sherpa cron container..."
 docker-compose build cron
 docker-compose stop cron
+docker-compose create cron
 docker-compose start cron
